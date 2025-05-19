7e265d666ec8727641da02d359e62ce3dd236c81
Sergey Starostin <starostin371@gmail.com>
2025-05-19 23:56:57 +0300

    Added models & UI & Input manager
    Imported models for prototype
    Added prototype
    Added simple UI
    Added InputManager to check user mouse clicks
    Added unit tests

---
+++ CityBuilderGame/Assets/EditModeTests/GridStructureTest.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GridStructureTest
+{
+    GridStructure structure;
+    [OneTimeSetUp]
+    public void Init()
+    {
+        structure = new GridStructure(3);
+    }
+    // A Test behaves as an ordinary method
+    [Test]
+    public void CalculateGridPositionPasses()
+    {
+        //Arrange
+        Vector3 position = new Vector3(0, 0, 0);
+        //Act
+        Vector3 returnPosition = structure.CalculateGridPosition(position);
+        //Assert
+        Assert.AreEqual(Vector3.zero, returnPosition);
+    }
+
+    [Test]
+    public void CalculateGridPositionFloatPasses()
+    {
+        //Arrange
+        Vector3 position = new Vector3(2.9f, 0, 2.9f);
+        //Act
+        Vector3 returnPosition = structure.CalculateGridPosition(position);
+        //Assert
+        Assert.AreEqual(Vector3.zero, returnPosition);
+    }
+
+    [Test]
+    public void CalculateGridPositionFails()
+    {
+        //Arrange
+        Vector3 position = new Vector3(3.1f, 0, 0);
+        //Act
+        Vector3 returnPosition = structure.CalculateGridPosition(position);
+        //Assert
+        Assert.AreNotEqual(Vector3.zero, returnPosition);
+    }
+}
---
+++ CityBuilderGame/Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask mouseInputMask;
+    //public GameObject buildingPrefab;
+
+    private Camera mainCamera;
+    private EventSystem eventSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        eventSystem = EventSystem.current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    public void GetInput()
+    {
+        if(Input.GetMouseButtonDown(0) && !eventSystem.IsPointerOverGameObject())
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                Vector3 position = hit.point - transform.position;
+            }
+        }
+    }
+    
+
+    /*private void CreateBuilding(Vector3 gridPosition)
+    {
+        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+    }*/
+}
